d33c1bb582b0b49cc3eb5ad17e7c3663afd0269a
ProstoTIP128 <152592547+ProstoTIP128@users.noreply.github.com>
2024-03-19 17:22:25 +0900

    Add files via upload

---
+++ jsjs.js
@@ -0,0 +1,44 @@
+function z1(){
+    document.getElementById("div").style.background = "red"
+}
+
+function z2(){
+    document.getElementById("div").style.background = "blue"
+}
+
+function z3(){
+    document.getElementById("div").style.background = "green"
+}
+
+function z4(){
+    document.getElementById("div").style.background = "orangered"
+}
+
+function z5(){
+    document.getElementById("div").style.background = "violet"
+}
+
+function z6(){
+    document.getElementById("div").style.background = "yellow"
+}
+
+function z7(){
+    document.getElementById("div").style.background = "darkgreen"
+}
+
+function z8(){
+    document.getElementById("div").style.background = "darkblue"
+}
+
+function z9(){
+    document.getElementById("div").style.background = "brown"
+}
+
+function z10(){
+    document.getElementById("div").style.backgroundImage = "linear-gradient(blue, green, yellow, darkviolet, orange)"
+}
+
+function z0(){
+    document.getElementById("div").style.background = "white"
+}
+
